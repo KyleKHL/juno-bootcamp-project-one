e2fc2247b390554472ea11ca4f745f4badc4d0d7
Kyle Lau <kyle.lau15@gmail.com>
2023-02-18 16:46:04 -0500

    the hamburger button is works

--- script.js
+++ script.js
@@ -3,15 +3,25 @@
     // this includes nav, ul, li
 
     const navElement = document.getElementsByClassName('navBar');
-    const ulElement = document.getElementsByClassName('menu');
-    const liElement = document.getElementsByClassName('menuItem');
+    const ulElement = document.querySelector('.menu');
+    const liElement = document.querySelectorAll('.menuItem');
     // make a variable for my button
     const menuButton = document.getElementsByClassName('hamburgerButton')[0];
+    console.log(ulElement)
 
-//3) add click event listener to button element that will open up the menu when user clicks it
+    // create a variable for media query
+    const mediaQuery = window.matchMedia('(max-width:750px)')
+//2) add click event listener to button element that will open up the menu when user clicks it
     // clickHandler function
-    clickHandler = function(event){
-        
+    clickHandler = function(){
+        if (ulElement.style.display === 'flex' && mediaQuery.matches){
+            ulElement.style.display = 'none';
+        } else {
+            ulElement.style.display = 'flex';
+            
+        }
     }
+
+    // event listener itself
     menuButton.addEventListener('click', clickHandler)
     
