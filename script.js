0cca0eadeaab8699116a98363205e28df5afadde
Kyle Lau <kyle.lau15@gmail.com>
2023-02-21 17:58:34 -0500

    removed some unneccesary pseudo code

--- script.js
+++ script.js
@@ -122,12 +122,5 @@
 
     rightButton.addEventListener('click', rightButtonClickHandler)
 
-// *************************************************
-// Code for dark mode toggle button
-// 1) create toggle button and pin it to end of navbar 
-// 2) create a click addEventListener for the button
-// 3) target the section elements to change the background color 
-// 3b) target to change body font color
-
 
 
