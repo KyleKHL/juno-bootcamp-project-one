39464efefb0c6a50b2ecd6bc686dfcd14e139854
Kyle Lau <kyle.lau15@gmail.com>
2023-03-16 21:30:40 -0400

    updated hamburger button and updated css partials

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-// create hamburger menu
-// create cariable for hamburger button
-const toggleHam = document.getElementsByClassName('toggleHamburger')[0];
-const menu = document.getElementsByClassName('menuLinks')[0];
-console.log(toggleHam)
-console.log(menu)
+// hamburger menu
+const burgerMenu = {};
 
-// write add event listener 
-toggleHam.addEventListener('click', function(){
-    menu.classList.toggle('menuLinks')
+burgerMenu.toggleHam = document.querySelector('#hamburgerButton');
+burgerMenu.menu = document.querySelector('#menuContainer');
+burgerMenu.burgerIcon = document.querySelector('#hamburgerIcon');
+burgerMenu.navLinks = document.querySelectorAll('.navLink');
+
+burgerMenu.toggleHam.addEventListener('click', function(event){
+    event.preventDefault();
+    burgerMenu.menu.classList.toggle('menuContainer');
+    burgerMenu.burgerIcon.classList.toggle('fa-bars');
+    burgerMenu.burgerIcon.classList.toggle('fa-xmark');
 })
 
-// *************************************************
-// Code for dark mode toggle button
-// 1) create toggle button with light and dark options and pin it to end of navbar.  
-// 2) create a click addEventListener for the button
-// 3) target the section elements to change the background color 
-// 3b) target to change body font color
-
+burgerMenu.navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        burgerMenu.menu.classList.toggle('menuContainer');
+    })
+});
